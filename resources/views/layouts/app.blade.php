a35d7309c7125a29781590af1c1910144ca85dd6
Gustavo Barragan <dev@droni.co>
2019-07-15 18:35:51 -0500

    migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,16 @@
 
 </head>
 <body>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143945159-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-143945159-1');
+</script>
+
   <script>
   window.fbAsyncInit = function() {
     FB.init({
